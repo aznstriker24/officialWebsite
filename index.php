fa86218c5f5ba2463e6a49757adab28a8a5f9e6f
Brian Shin <bys8@cornell.edu>
2016-03-24 20:29:20 -0400

    blaah

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
     <body>
         <h1>Brian Shin</h1>
         <h2>some catchy tagline</h2>
-        <?php echo "<p>hello world</p>" ?>
         <?php
-            for ($i = 0; $i < 100; $i++) {
-                echo "<p>An example of a natural number is {$i}.</p>";
+            echo "<p>Here's a list of a couple fibonacci numbers!</p>"
+            $i = 0;
+            $j = 1;
+            while ($j < 1000) {
+                echo "<p>{$i}</p>";
             }
         ?>
     </body>
