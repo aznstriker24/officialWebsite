7eb5b52eca886401f030a1cca72749b5e1cb8789
Brian Shin <bys8@cornell.edu>
2016-03-24 20:25:04 -0400

    phpppp

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
         <h1>Brian Shin</h1>
         <h2>some catchy tagline</h2>
         <?php echo "<p>hello world</p>" ?>
+        <?php
+            for ($i = 0; $i < 100; $i++) {
+                echo "<p>An example of a natural number is {$i}.</p>";
+            }
+        ?>
     </body>
 </html>
