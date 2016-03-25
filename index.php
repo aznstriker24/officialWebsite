5a906abf660ff4758b0275817414344c683db8db
Brian Shin <bys8@cornell.edu>
2016-03-24 20:33:36 -0400

    blaah

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
         <h2>some catchy tagline</h2>
         <?php
             echo "<p>Here's a list of a couple fibonacci numbers!</p>";
+            echo "<p>";
             $i = 0;
             $j = 1;
             while ($j < 1000) {
-                echo "<p>{$i}</p>";
+                echo "{$i}\t";
                 $j = $j + $i;
                 $i = $j - $i;
             }
+            echo "</p>";
         ?>
     </body>
 </html>
