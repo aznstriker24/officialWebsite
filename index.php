62a9ac147c4ca53f682735add799d4c441ab74d0
Brian Shin <bys8@cornell.edu>
2016-03-27 11:26:49 -0400

    starting to use yahoo's PURE for the front-end crap

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
         <title>Brian Shin</title>
     </head>
