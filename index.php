d1db2a06ff3d8e3a8954a84862f80921a6f750c1
Brian Shin <bys8@cornell.edu>
2016-03-27 11:19:05 -0400

    starting to use yahoo's PURE for the front-end crap

--- index.php
+++ index.php
@@ -19,5 +19,11 @@
             }
             echo "</p>";
         ?>
+
+        <div class="pure-g">
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+        </div>
     </body>
 </html>
