6ed361d74aea6bae5d4bfc343f10f9d890114a56
Brian Shin <bys8@cornell.edu>
2016-03-30 00:26:20 -0400

    asdfasdfa

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
         <!-- Navigation bar -->
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+
                 <ul class="nav navbar-nav navbar-left">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Blog</a></li>
