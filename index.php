53e8ed916cece454f31a572dc8b5908efccec8cb
Brian Shin <bys8@cornell.edu>
2016-03-29 22:18:57 -0400

    working on home page

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
         <title>Home | Brian Shin</title>
     </head>
     <body>
+        <nav>
+            <ul>
+                <li>asdfa</li>
+                <li>asdfasdfds</li>
+            </ul>
+        </nav>
+        
         <header>
             <h1>Brian Shin</h1>
             <h2>just another nerd</h2>
