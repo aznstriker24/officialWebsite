9904ffd963a9b6401323d5e679d32ec0850d3fff
Brian Shin <bys8@cornell.edu>
2016-03-29 22:35:40 -0400

    booooootstrap.

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <title>Home | Brian Shin</title>
     </head>
     <body>
-        <nav>
-            <ul>
-                <li>asdfa</li>
-                <li>asdfasdfds</li>
-            </ul>
+        <nav class="nav">
+            <div class="pull-left">
+                <ul>
+                    <li>Home</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+
+            <div class="pull-right">
+                <ul>
+                    <li>asdfa</li>
+                    <li>asdfas</li>
+                    <li>sdfdsasdf</li>
+                </ul>
+            </div>
         </nav>
-        
+
         <header>
             <h1>Brian Shin</h1>
             <h2>just another nerd</h2>
