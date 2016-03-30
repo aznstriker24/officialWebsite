dd52b8a4fea55a4eaf75aba452aedf4e26ccf357
Brian Shin <bys8@cornell.edu>
2016-03-29 22:16:13 -0400

    dsfasdf

--- index.php
+++ index.php
@@ -4,19 +4,9 @@
         <title>Home | Brian Shin</title>
     </head>
     <body>
-
-
-        <?php
-            echo "<p>Here's a list of a couple fibonacci numbers!</p>";
-            echo "<p>";
-            $i = 0;
-            $j = 1;
-            while ($j < 1000000000) {
-                echo "{$i},\t\t";
-                $j = $j + $i;
-                $i = $j - $i;
-            }
-            echo "</p>";
-        ?>
+        <header>
+            <h1>Brian Shin</h1>
+            <h2>just another nerd</h2>
+        </header>
     </body>
 </html>
