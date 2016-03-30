84de875f12eea047fff03f24bd44637532e54135
Brian Shin <bys8@cornell.edu>
2016-03-29 22:48:32 -0400

    Homepage

--- index.php
+++ index.php
@@ -5,26 +5,52 @@
         <title>Home | Brian Shin</title>
     </head>
     <body>
+        <!-- Navigation bar -->
         <div class="nav">
-            <div class="pull-left">
-                <ul>
-                    <li>Home</li>
-                    <li>Blog</li>
-                </ul>
-            </div>
+            <div class="container">
+                <div class="pull-left">
+                    <ul>
+                        <li>Home</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
 
-            <div class="pull-right">
-                <ul>
-                    <li>asdfa</li>
-                    <li>asdfas</li>
-                    <li>sdfdsasdf</li>
-                </ul>
+                <div class="pull-right">
+                    <ul>
+                        <li>asdfa</li>
+                        <li>asdfas</li>
+                        <li>sdfdsasdf</li>
+                    </ul>
+                </div>
             </div>
         </div>
 
-        <header>
-            <h1>Brian Shin</h1>
-            <h2>just another nerd</h2>
-        </header>
+        <!-- Jumbotron!! -->
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Brian Shin</h1>
+                <h2>just another nerd</h2>
+            </div>
+        </div>
+
+        <!-- blah blah -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h1>Heading 1</h1>
+                    <p>content 1</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h1>Heading 2</h1>
+                    <p>content 2</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h1>Heading 3</h1>
+                    <p>content 3</p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
