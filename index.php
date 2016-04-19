5f5562b215c5d952ed4dc0a186b0a0a4799f52a0
Brian Shin <bys8@cornell.edu>
2016-04-19 19:41:30 -0400

    Refactored stuff

--- index.php
+++ index.php
@@ -1,113 +1,67 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+    include('header.html');
+?>
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <link rel="stylesheet" href="main.css">
+<div class="contents">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h1>Heading 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Mauris nec malesuada
+                libero. Phasellus sit amet libero nec lacus
+                vestibulum interdum. In cursus purus at feugiat
+                pharetra. Phasellus sagittis, tortor ac mollis
+                posuere, ante tortor maximus dui, a pretium felis
+                erat nec odio. Duis eu egestas libero, vitae
+                iaculis leo. Morbi ultrices sagittis odio, ut
+                dignissim turpis molestie et. Donec ut odio sed
+                purus sodales consequat at sed mi. Cras feugiat
+                magna dui, sed porta odio fringilla nec. Fusce
+                rhoncus laoreet feugiat. Morbi aliquam eu ante eu
+                accumsan. Donec sed tincidunt urna, vitae euismod
+                justo.</p>
+            </div><!-- col-md-4 -->
 
-        <title>Home | Brian Shin</title>
-    </head>
-    <body>
-        <!-- Navigation bar -->
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="">Brian Shin</a>
-                </div>
+            <div class="col-md-4">
+                <h1>Heading 2</h1>
+                <p>Curabitur ac quam odio. Suspendisse sit amet
+                vestibulum urna. Mauris semper nulla erat,
+                fermentum efficitur neque sodales
+                viverra. Phasellus dui urna, consequat vel
+                suscipit quis, commodo nec velit. Quisque congue
+                varius quam et eleifend. Maecenas sit amet nulla
+                ac justo rutrum tempor ac ut eros. Nam interdum
+                elit sit amet imperdiet porttitor. Suspendisse
+                lacus lacus, luctus egestas libero a, hendrerit
+                finibus eros. Donec posuere est ac nulla faucibus
+                interdum. Fusce ullamcorper vulputate nisl, ut
+                suscipit ligula scelerisque sit amet. Nullam eu
+                erat rutrum justo varius vestibulum fermentum eget
+                mi. Nam condimentum nisi id purus vestibulum
+                laoreet. Sed id sem orci. Aliquam magna orci,
+                fermentum a dictum in, aliquet eu purus. Nam
+                ullamcorper porttitor turpis, a pulvinar sem
+                tincidunt vitae.</p>
+            </div><!-- col-md-4 -->
 
-                <div class="collapse navbar-collapse" id="navbar-collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li><a href="#">Blog</a></li>
-                    </ul>
+            <div class="col-md-4">
+                <h1>Heading 3</h1>
+                <p>Ut tempor ut dui venenatis congue. Proin
+                molestie felis vel metus luctus accumsan. Cras
+                enim lorem, euismod in orci eu, dictum molestie
+                felis. Phasellus a aliquam metus. Vestibulum ante
+                ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae; Nullam quis felis in sem
+                ultricies suscipit. Curabitur rutrum scelerisque
+                dolor. Aenean at ligula vel mi malesuada
+                mollis. Vestibulum mattis ultricies elit, id
+                luctus tellus mollis rutrum.</p>
+            </div><!-- col-md-4 -->
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- contents -->
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="https://www.instagram.com/aznstriker24">Instagram</a></li>
-                        <li><a href="https://www.github.com/aznstriker24">GitHub</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <!-- Jumbotron!! -->
-        <div class="jumbotron">
-            <div class="container">
-                <h1>Brian Shin</h1>
-                <h2>just another nerd</h2>
-            </div><!-- container -->
-        </div><!-- jumbotron -->
-
-        <!-- blah blah -->
-        <div class="contents">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h1>Heading 1</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Mauris nec malesuada
-                        libero. Phasellus sit amet libero nec lacus
-                        vestibulum interdum. In cursus purus at feugiat
-                        pharetra. Phasellus sagittis, tortor ac mollis
-                        posuere, ante tortor maximus dui, a pretium felis
-                        erat nec odio. Duis eu egestas libero, vitae
-                        iaculis leo. Morbi ultrices sagittis odio, ut
-                        dignissim turpis molestie et. Donec ut odio sed
-                        purus sodales consequat at sed mi. Cras feugiat
-                        magna dui, sed porta odio fringilla nec. Fusce
-                        rhoncus laoreet feugiat. Morbi aliquam eu ante eu
-                        accumsan. Donec sed tincidunt urna, vitae euismod
-                        justo.</p>
-                    </div><!-- col-md-4 -->
-
-                    <div class="col-md-4">
-                        <h1>Heading 2</h1>
-                        <p>Curabitur ac quam odio. Suspendisse sit amet
-                        vestibulum urna. Mauris semper nulla erat,
-                        fermentum efficitur neque sodales
-                        viverra. Phasellus dui urna, consequat vel
-                        suscipit quis, commodo nec velit. Quisque congue
-                        varius quam et eleifend. Maecenas sit amet nulla
-                        ac justo rutrum tempor ac ut eros. Nam interdum
-                        elit sit amet imperdiet porttitor. Suspendisse
-                        lacus lacus, luctus egestas libero a, hendrerit
-                        finibus eros. Donec posuere est ac nulla faucibus
-                        interdum. Fusce ullamcorper vulputate nisl, ut
-                        suscipit ligula scelerisque sit amet. Nullam eu
-                        erat rutrum justo varius vestibulum fermentum eget
-                        mi. Nam condimentum nisi id purus vestibulum
-                        laoreet. Sed id sem orci. Aliquam magna orci,
-                        fermentum a dictum in, aliquet eu purus. Nam
-                        ullamcorper porttitor turpis, a pulvinar sem
-                        tincidunt vitae.</p>
-                    </div><!-- col-md-4 -->
-
-                    <div class="col-md-4">
-                        <h1>Heading 3</h1>
-                        <p>Ut tempor ut dui venenatis congue. Proin
-                        molestie felis vel metus luctus accumsan. Cras
-                        enim lorem, euismod in orci eu, dictum molestie
-                        felis. Phasellus a aliquam metus. Vestibulum ante
-                        ipsum primis in faucibus orci luctus et ultrices
-                        posuere cubilia Curae; Nullam quis felis in sem
-                        ultricies suscipit. Curabitur rutrum scelerisque
-                        dolor. Aenean at ligula vel mi malesuada
-                        mollis. Vestibulum mattis ultricies elit, id
-                        luctus tellus mollis rutrum.</p>
-                    </div><!-- col-md-4 -->
-                </div><!-- row -->
-            </div><!-- container -->
-        </div><!-- contents -->
-
-        <!-- idk wtf jquery? wtf -->
-        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    </body>
-</html>
+<?php
+    include('footer.html');
+?>
